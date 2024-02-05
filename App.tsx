5924dec68cc2e59db6c4495309577849bc73606f
kochq <matukoch@live.com>
2024-02-04 22:52:21 -0300

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { supabase } from './lib/supabase';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            const { data, error } = await supabase.from("recipes").select("*");
+
+            if(error) {
+                console.log(error);
+            } else {
+                setRecipes(data);
+            }
+        }
+
+        fetchRecipes();
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <Text>Open p App.tsx to stat working on your app!</Text>
+            <StatusBar style="auto" />
+            <FlatList
+                data={recipes}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => <Text>{item.title}</Text>}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
